e096303dae6337bcb03e23563dc7b5b471ae9738
hallgato <hallgato@LABOR220-22>
2024-03-14 15:49:41 +0100

    planeview

---
+++ WpfApp/WpfApp/PlaneViewModel.cs
@@ -0,0 +1,38 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp
+{
+    internal class PlaneViewModel : ObservableObject
+    {
+		private string name;
+
+		public string Name
+		{
+			get { return name; }
+			set {
+				SetProperty(ref name, value);
+				OnPropertyChanged();
+				}
+		}
+
+		private string failure;
+
+		public string Failure
+		{
+			get { return failure; }
+			set {
+                SetProperty(ref failure, value);
+                OnPropertyChanged();
+            }
+		}
+
+
+
+
+	}
+}
