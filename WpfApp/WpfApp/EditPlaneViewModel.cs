9a0511afb3f201896ef211d62c91a4f9d5ac1ef4
hallgato <hallgato@LABOR220-22>
2024-03-14 17:31:46 +0100

    sefes

---
+++ WpfApp/WpfApp/EditPlaneViewModel.cs
@@ -0,0 +1,31 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp
+{
+    public class EditPlaneViewModel : ObservableRecipient
+    {
+		private PlaneViewModel newPlane;
+
+		public PlaneViewModel NewPlane
+		{
+			get { return newPlane; }
+			set { SetProperty(ref newPlane, value);
+				OnPropertyChanged();
+			}
+		}
+
+        public EditPlaneViewModel(PlaneViewModel newPlane)
+        {
+            NewPlane = newPlane;
+        }
+
+        public EditPlaneViewModel()
+        {
+        }
+    }
+}
